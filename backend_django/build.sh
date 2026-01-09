55008eae87de2a7dcb0b30b00345a193e61603a6
luishueda <huedaluis72@gmail.com>
2026-01-09 04:08:22 -0300

    Fix JSON encoding for loaddata

--- backend_django/build.sh
+++ backend_django/build.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Cargar datos iniciales solo si la base de datos está vacía
+# Cargar datos iniciales
 python manage.py loaddata datos_iniciales.json --ignorenonexistent || true
