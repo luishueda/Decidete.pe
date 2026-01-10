018e379c11caa30f6c7636a680c6acd16093287e
luishueda <huedaluis72@gmail.com>
2026-01-10 02:08:16 -0300

    Fix: Use correct environment import in partidos service

--- src/app/core/services/partidos.service.ts
+++ src/app/core/services/partidos.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
